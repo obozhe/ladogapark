59702df68726014e6bb3c5fdab47df35f9533752
obozhe <ava.narek@gmail.com>
2023-07-31 23:12:18 -0500

    fix

--- src/components/ui/AccordionTransition.tsx
+++ src/components/ui/AccordionTransition.tsx
@@ -3,17 +3,16 @@ import { Transition } from '@headlessui/react';
 
 type Props = { show: boolean; children: ReactNode; maxHeight?: number };
 
-export default function AccordionTransition({ show, children, maxHeight = 300 }: Props) {
-  const maxHeightClass = `max-h-[${maxHeight}px]`;
+export default function AccordionTransition({ show, children }: Props) {
   return (
     <Transition
       as={Fragment}
       show={show}
       enter="ease-out duration-300"
       enterFrom="max-h-0"
-      enterTo={maxHeightClass}
+      enterTo="max-h-[300px]"
       leave="ease-in duration-300"
-      leaveFrom={maxHeightClass}
+      leaveFrom="max-h-[300px]"
       leaveTo="max-h-0"
     >
       <div className="overflow-hidden">{children}</div>
